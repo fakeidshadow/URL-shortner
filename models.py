6f1c0a0f7f452efa5aa490140fa92c8e4815a2d4
fakeidshadow <mmghasemi13@gmail.com>
2025-12-12 15:29:18 +0330

    feat: add url table

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+metadata = Base.metadata
+
+class Url(Base):
+    __tablename__ = 'url'
+
+    id = Column(Integer, primary_key=True)
+    address = Column(String(100), unique=True)
+    code = Column(String(100), unique=True)
+    date = Column(String(50))
